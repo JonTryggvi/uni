cdbf24e6563aa951321156fa73b8a57738a66236
JonTryggvi <jontryggvi@jontryggvi.is>
2019-03-02 20:55:40 +0100

    setting up components for js scss and php

--- library/uni-partials/component-header.php
+++ library/uni-partials/component-header.php
@@ -1,8 +1,11 @@
-<header>
-	<nav>
-		<?php foundationpress_top_bar_r(); ?>
-
-	</nav>
-
+<header class="uni_header">
+	<div class="uni_header__inner-container grid-container">
+		<div class="uni_header__logo-container">
+			<a href="<?php echo home_url(); ?>" class="uni_header__logo-link">Uni</a>
+		</div>
+		<nav>
+			<?php foundationpress_top_bar_r('uni_header__nav-list'); ?>
+		</nav>
+</div>
 </header>
 
---
+++ library/uni-partials/component-hero.php
@@ -0,0 +1,13 @@
+<section class="uni_section uni_section__hero " role="main" >
+
+		<div <?php post_class('grid-container'); ?> id="post-<?php the_ID(); ?>">
+			<div class="entry-content grid-x">
+
+				<h1 class="medium-5 "><?php echo $hero_title; ?></h1>
+			</div>
+
+
+
+	</div>
+
+</section>
